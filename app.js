7ebe8fe0f3af39a25c25fb9a4ed1731074966e06
miracle0403 <lawrencemiracle71@gmail.com>
2018-04-29 05:38:19 -0700

    newfles

---
+++ app.js
@@ -0,0 +1,91 @@
+var createError = require('http-errors');
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+var bodyParser = require('body-parser');
+var expressValidator = require('express-validator');
+var bcrypt = require('bcrypt');
+var mysql = require('mysql');
+
+//Authentication packages
+var passport = require('passport');
+var localStrategy = require('passport-local'), Strategy;
+var session = require('express-session');
+var MySQLStore = require ('express-mysql-session')(session);
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+var db = require('./db.js');
+
+var app = express();
+
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+var options = {
+  host: "localhost",
+  user: "swift", 
+  password: "Swift1",
+  database: "miracledb"
+};
+
+var sessionStore = new MySQLStore(options);
+  
+app.use(session({
+  secret: 'hfgdhfjdhfvg',
+  resave: false,
+  store: sessionStore,
+  saveUninitialized: false,
+  /**cookie:
+    #secure: true**/
+  }));
+app.use(passport.initialize());
+app.use(passport.session());
+
+// middle ware
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(expressValidator());
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+passport.use(new localStrategy(function(username, password, done){
+    console.log(username)
+    console.log(password)
+
+    const db = require('./db.js');
+
+    db.query('SELECT password FROM test WHERE username = ?', [username], function (err, results, fields){
+      if (err) {done(err)};
+
+      if (results.length === 0){
+        done(null, false);
+      }
+      done(null, 'ghfghh');
+    })
+    return done(null, 'fdgd');
+}))
+// catch 404 and forward to error handler
+app.use(function(req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function(err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
---
+++ routes/index.js
@@ -0,0 +1,106 @@
+var express = require('express');
+var passport = require('passport')
+var router = express.Router();
+
+var expressValidator = require('express-validator');
+
+var bcrypt = require('bcrypt');
+const saltRounds = 15;
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  console.log(req.user)
+  console.log(req.isAuthenticated())
+  res.render('index', { title: 'SWIFT CIRCLE' });
+});
+
+//register get request
+router.get('/register', function(req, res, next) {
+  res.render('register', { title: 'REGISTRATION' });
+});
+
+//get login
+router.get('/login', function(req, res, next) {
+  res.render('login', { title: 'LOG IN' });
+});
+
+//get profile
+router.get('/profile', authentificationMiddleware(), function(req, res, next) {
+  res.render('profile', { title: 'USER PPROFILE' });
+});
+
+//post register
+router.post('/register', function(req, res, next) {
+  console.log(req.body)
+  req.checkBody('sponsor', 'Sponsor must not be empty').notEmpty();
+  req.checkBody('sponsor', 'Sponsor must be between 8 to 25 characters').len(8,25);
+  req.checkBody('username', 'Username must be between 8 to 25 characters').len(8,25);
+  req.checkBody('pass1', 'Password must be between 8 to 25 characters').len(8,100);
+  req.checkBody('pass2', 'Password confirmation must be between 8 to 100 characters').len(8,100);
+  req.checkBody('email', 'Email must be between 8 to 25 characters').len(8,25);
+  req.checkBody('email', 'Invalid Email').isEmail();
+  req.checkBody('pass1', 'Password must match').equals(req.body.pass2);
+  //req.checkBody('pass1', 'Password must have upper case, lower case, symbol, and number').matches(/^(?=,*\d)(?=, *[a-z])(?=, *[A-Z])(?!, [^a-zA-Z0-9]).{8,}$/, "i")
+ 
+  var errors = req.validationErrors();
+
+  if (errors) {
+    console.log(JSON.stringify(errors));
+    res.render('register', { title: 'REGISTRATION FAILED', errors: errors});
+    //return noreg
+  }
+  else {
+    var username = req.body.username
+    var password = req.body.pass1
+    var cpass = req.body.pass2
+    var email = req.body.email
+    var sponsor = req.body.sponsor
+
+  /**var noreg = function(reg){
+    reg == false
+  }*/
+ 
+    var db = require('../db.js');
+    bcrypt.hash(password, saltRounds, function(err, hash){
+      db.query('INSERT INTO test (username, email, sponsor, password, status) VALUES (?, ?, ?, ?, ?)', [username, email, sponsor, hash, 0], function(error, result, fields){
+        if (error) throw error;
+        else{
+          db.query('SELECT LAST_INSERT_ID() as user_id', function(error, results, fields){
+            if (error) throw error;
+  
+            var user_id = results[0];
+            console.log(results[0])
+            req.login(user_id, function(err){
+              res.redirect('profile')
+              console.log('Registration was a success')
+            });
+          });
+        }
+        
+      });
+    });
+  }
+});
+//Passport login
+passport.serializeUser(function(user_id, done){
+  done(null, user_id)
+});
+        
+passport.deserializeUser(function(user_id, done){
+  done(null, user_id)
+});
+
+//authentication middleware snippet
+function authentificationMiddleware(){
+  return (req, res, next) => {
+    console.log(JSON.stringify(req.session.passport));
+  if (req.isAuthenticated()) return next();
+
+  res.redirect('/login'); 
+  }
+}
+router.post('/login', passport.authenticate('local', {
+  successRedirect: '/profile',
+  failureRedirect: '/login'
+}));
+module.exports = router;
